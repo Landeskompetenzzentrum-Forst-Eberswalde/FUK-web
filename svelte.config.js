2d8260222158932c959350287e7343fbed9234d1
Gerrit Balindt <gerrit.balindt@gruenecho.de>
2025-02-09 23:25:12 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		},
 		define: {
 			'process.env.VITE_ICP_ANON_KEY': JSON.stringify(process.env.VITE_ICP_ANON_KEY),
-			'process.env.VITE_ICP_SERVER_URL': JSON.stringify(process.env.VITE_ICP_SERVER_URL),
+			'process.env.VITE_ICP_SERVER_URL': JSON.stringify(process.env.VITE_ICP_SERVER_URL)
 		},
 	}
 };
